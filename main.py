d33b42e9ee2b4931398edbdb1c5349ba64da2a68
ASquaredM <43936923+ProNoobWarning@users.noreply.github.com>
2020-05-22 05:42:55 +0200

    UI naming and edits to main

--- main.py
+++ main.py
@@ -12,9 +12,25 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
     def __init__(self,mainWindow):
         super(ApplicationWindow, self).setupUi(mainWindow)
 
+        ''' Buttons
         self.AddDevice_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
-        
-        
+        self.SubmitAnswers_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        self.CreateForm_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        self.ManageTasks_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        self.Export_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        self.Print_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        '''
+        ''' Tables
+        self.Forms_table
+        self.Department_table
+        self.Devices_table
+        self.ToDo_table
+        '''
+        ''' LineEdit
+        self.Search_lineEdit
+        '''
+
+
 
     def InsertAtIndex(self, table, y, x, Item):
         table.setItem(y, x, QTableWidgetItem(Item))
