3aae43e5c75fbd9c4d26d6f1d819666d49f55faa
ASquaredM <43936923+ProNoobWarning@users.noreply.github.com>
2020-05-22 05:13:34 +0200

    Edits to GUI and main

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import os
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
 
-from hospital_gui import Ui_MainWindow
+from DB_Management import DatabaseUtilities as DU
+import hospital_gui
 
 
-class ApplicationWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(ApplicationWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+class ApplicationWindow(hospital_gui.Ui_MainWindow):
+    def __init__(self,mainWindow):
+        super(ApplicationWindow, self).setupUi(mainWindow)
+
+        self.AddDevice_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
+        
+        
+
+    def InsertAtIndex(self, table, y, x, Item):
+        table.setItem(y, x, QTableWidgetItem(Item))
 
 
 def main():
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     app = QtWidgets.QApplication(sys.argv)
-    application = ApplicationWindow()
-    application.show()
-    app.exec_()
+    mainWindow = QtWidgets.QMainWindow()
+    ApplicationWindow(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
