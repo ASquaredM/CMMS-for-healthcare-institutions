369accfe9f5ebc2c696c65da3ba2c1ce8aaa5f71
ASquaredM <43936923+ProNoobWarning@users.noreply.github.com>
2020-05-24 16:16:42 +0200

    Removing Useless commented lines/ Adding Action

--- main.py
+++ main.py
@@ -15,39 +15,19 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
     def __init__(self, mainWindow):
         super(ApplicationWindow, self).setupUi(mainWindow)
         ''' Buttons
-        self.AddDevice_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
         self.SubmitAnswers_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
-        self.CreateForm_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
         self.ManageTasks_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
         self.Export_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
         self.Print_button.clicked.connect(lambda: self.InsertAtIndex(self.Devices_table, 1, 0, 'Test'))
         '''
         ''' Tables
-        self.Forms_table
-        self.Department_table
-        self.Devices_table
         self.ToDo_table
         '''
         ''' LineEdit
         self.Search_lineEdit
         '''
-        ''' Actions (Ordered by the actual toolbar order)
-        self.actionFollow_Up.triggered.connect()
-        self.actionHome.triggered.connect()
-        self.actionTo_Do.triggered.connect()
-        self.actionDaily_Inspection.triggered.connect()
-        self.actionInformation.triggered.connect()
-        self.actionTools.triggered.connect()
-        self.actionAdd_Device.triggered.connect()
-        self.actionCreate_Form.triggered.connect()
-        self.actionManage_Tasks.triggered.connect()
-        self.actionCMMS.triggered.connect()
-        '''
         ''' Combos
-        self.DepartmentSelection_combo
-        self.DepartmentSelection_combo_2
         self.Date_comboBox
-        self.Inspection_comboBox
         '''
 
         self.CurrDate = datetime.now().strftime("%y-%m-%d")
@@ -62,10 +42,10 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
             self.q8, self.q9, self.q10
         ]
         self.question2 = [
-            self.q12, self.q12, self.q13, self.q14, self.q15, self.q16, self.q17,
-            self.q18, self.q19, self.q20
+            self.q11, self.q12, self.q13, self.q14, self.q15, self.q16,
+            self.q17, self.q18, self.q19, self.q20
         ]
-        self.questions = [self.question1 , self.question2]
+        self.questions = [self.question1, self.question2]
         self.InitCheckBoxes()
         self.InitActions()
         self.UpdateDevCombo()
@@ -84,20 +64,20 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
             self.checkq15, self.checkq16, self.checkq17, self.checkq18,
             self.checkq19, self.checkq20
         ]
-        self.checks = [self.checks1 , self.checks2]
+        self.checks = [self.checks1, self.checks2]
         self.CheckAll_checkBox.stateChanged.connect(self.CheckAll)
 
         for i in range(10):
             self.checks[0][i].stateChanged.connect(self.FormIsFilling)
 
     def InitActions(self):
+        #Ordered by the actual toolbar order
         self.actionFollow_Up.triggered.connect(
             lambda: self.toolBox.setCurrentIndex(0))
         self.actionHome.triggered.connect(lambda: self.NavTo(0))
         self.actionTo_Do.triggered.connect(lambda: self.NavTo(1))
         self.actionDaily_Inspection.triggered.connect(lambda: self.NavTo(2))
         self.actionPPM.triggered.connect(lambda: self.NavTo(3))
-
         self.actionInformation.triggered.connect(
             lambda: self.toolBox.setCurrentIndex(1))
         self.actionTools.triggered.connect(
@@ -106,6 +86,7 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
             self.dockWidget_AddDeviceWindow.show)
         self.actionCreate_Form.triggered.connect(
             self.dockWidget_CreateFormWindow.show)
+        self.actionManage_Tasks.triggered.connect(lambda: self.NavTo(2))
         self.actionCMMS.triggered.connect(self.dockWidget_AboutWindow.show)
 
     def InitComboBoxes(self):
@@ -124,8 +105,6 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
         self.Inspection_comboBox.currentIndexChanged.connect(self.UpdateForm)
         self.ppm_comboBox.currentIndexChanged.connect(self.update_form2)
 
-        
-
     def InitButtons(self):
         self.AddDevice_button.clicked.connect(
             self.dockWidget_AddDeviceWindow.show)
@@ -136,10 +115,12 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
         self.Inspection_comboBox.clear()
         self.ppm_comboBox.clear()
         dailyDevices_names = DB.RunCommand("SELECT DevName ,DevId FROM device")
-        dailyDevices_names = [str(device[0])+" ,ID:"+str(device[1]) for device in dailyDevices_names]
-        self.Inspection_comboBox.addItems(dailyDevices_names) 
-        self.ppm_comboBox.addItems(dailyDevices_names) 
-
+        dailyDevices_names = [
+            str(device[0]) + " ,ID:" + str(device[1])
+            for device in dailyDevices_names
+        ]
+        self.Inspection_comboBox.addItems(dailyDevices_names)
+        self.ppm_comboBox.addItems(dailyDevices_names)
 
     def UpdateTables(self):
         self.UpdateTable(DB.GetRows('department'), self.Department_table)
@@ -163,8 +144,8 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
 
     def UpdateForm(self):
         family = DB.SelectRows(
-            "device",
-            "DevName='{}'".format(str(self.Inspection_comboBox.currentText()).split(" ,ID:")[0]))
+            "device", "DevName='{}'".format(
+                str(self.Inspection_comboBox.currentText()).split(" ,ID:")[0]))
         # print(str(self.Inspection_comboBox.currentText()).split(" ,ID:")[0])
         # print(family)
         if len(family) > 0:
@@ -183,23 +164,22 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
                         self.question1[i - 1].setText(quest)
                     else:
                         EnableVar -= 1
-                self.clear_form(0,len(self.checks1), 10 - EnableVar)
-                self.enable_form(0,EnableVar) #######
+                self.clear_form(0, len(self.checks1), 10 - EnableVar)
+                self.enable_form(0, EnableVar)  #######
             else:
-                self.clear_form(0,10, 10)
+                self.clear_form(0, 10, 10)
                 self.question1[0].setText('No Form To Display For This Device')
         else:
-            self.clear_form(0,10, 10)
+            self.clear_form(0, 10, 10)
             self.question1[0].setText('No Form To Display For This Device')
 
     def update_form2(self):
         family = DB.SelectRows(
-            "device",
-            "DevName='{}'".format(str(self.ppm_comboBox.currentText()).split(" ,ID:")[0]))
+            "device", "DevName='{}'".format(
+                str(self.ppm_comboBox.currentText()).split(" ,ID:")[0]))
         if len(family) > 0:
             form = DB.SelectRows(
-                "form",
-                "formfamily='{}' AND formtype= 'PPM' ".format(
+                "form", "formfamily='{}' AND formtype= 'PPM' ".format(
                     str(family[0][8])))
             if len(form) > 0:
                 form = form[0][3].split("?")
@@ -210,26 +190,27 @@ class ApplicationWindow(hospital_gui.Ui_MainWindow):
                         self.question2[i - 1].setText(quest)
                     else:
                         EnableVar -= 1
-                self.clear_form(1,len(self.checks2), 10 - EnableVar)  ############generalize these functions ---> enable_form(self,checklist, amount) , clear_Form(self, questionList,checklist,lenlist, amount)
-                self.enable_form(1,EnableVar) #######
+                self.clear_form(
+                    1, len(self.checks2), 10 - EnableVar
+                )  ############generalize these functions ---> enable_form(self,checklist, amount) , clear_Form(self, questionList,checklist,lenlist, amount)
+                self.enable_form(1, EnableVar)  #######
             else:
-                self.clear_form(1,10, 10)
+                self.clear_form(1, 10, 10)
                 self.question2[0].setText('No Form To Display For This Device')
         else:
-            self.clear_form(1,10, 10)
+            self.clear_form(1, 10, 10)
             self.question2[0].setText('No Form To Display For This Device')
-
 
     def InsertAtIndex(self, table, y, x, Item):
         table.setItem(y, x, QTableWidgetItem(Item))
 
-    def clear_form(self, group,lenlist, amount):
+    def clear_form(self, group, lenlist, amount):
         for i in range(amount):
             self.questions[group][lenlist - i - 1].clear()
             self.checks[group][lenlist - i - 1].hide()
             self.checks[group][lenlist - i - 1].setDisabled(True)
 
-    def enable_form(self, group,amount):
+    def enable_form(self, group, amount):
         for i in range(amount):
             self.checks[group][i].setDisabled(False)
             self.checks[group][i].show()
